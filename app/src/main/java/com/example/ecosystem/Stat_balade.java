a055a8eae32e221424404378a107e6834867f0cb
gaga63 <37262851+gaga63@users.noreply.github.com>
2020-05-09 10:38:01 +0200

    Add files via upload

--- app/src/main/java/com/example/ecosystem/Stat_balade.java
+++ app/src/main/java/com/example/ecosystem/Stat_balade.java
@@ -2,19 +2,20 @@ package com.example.ecosystem;
 
 import android.content.Intent;
 import android.os.Bundle;
-import java.util.Calendar;
-
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class Stat_balade extends AppCompatActivity{
 
+    CalendarView calendarView;
+    TextView dateDisplay;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stat_balade);
 
@@ -22,6 +23,26 @@ public class Stat_balade extends AppCompatActivity{
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
         bottomNavigationView.setSelectedItemId(R.id.imageButton);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        calendarView = (CalendarView) findViewById(R.id.calendrier);
+        dateDisplay = (TextView) findViewById(R.id.date);
+        dateDisplay.setText("Date: ");
+
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                if ((i2 == 2) || (i2 == 9) || (i2 == 16) || (i2 == 23) || (i2 == 30) && i1 == 4){
+                    dateDisplay.setText("Date: " + i2 + " / " + (i1+1) + " / " + i + "\n\n" + "C'est le jour de la collecte des poubelles jaunes : emballages, journaux, magazines.");
+                }
+                else if ((i2 == 4) || (i2 == 7) ||(i2 == 11) || (i2 == 14) || (i2 == 18) || (i2 == 21) || (i2 == 25) || (i2 == 28) && i1 == 4){
+                    dateDisplay.setText("Date: " + i2 + " / " + (i1+1) + " / " + i + "\n\n" + "C'est le jour de la collecte des poubelles marrons : ordures mÃ©nagÃ¨res.");
+                }
+                else{
+                    dateDisplay.setText("Date: " + i2 + " / " + (i1+1) + " / " + i + "\n\n" + "Il n'y a aucune collecte aujourd'hui.");
+                }
+            }
+        });
     }
 
 

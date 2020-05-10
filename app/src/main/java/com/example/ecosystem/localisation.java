8d71c8555c0fea44480af0a0a088ac03a017a378
johannsubirats <44833499+johannsubirats@users.noreply.github.com>
2020-05-10 12:06:15 +0200

    carte beau chemin

--- app/src/main/java/com/example/ecosystem/localisation.java
+++ app/src/main/java/com/example/ecosystem/localisation.java
@@ -67,20 +67,54 @@ public class localisation extends FragmentActivity implements OnMapReadyCallback
         Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
                 .clickable(true)
                 .add(
-                        new LatLng(48.7774, 2.3532) ,
+                        new LatLng(48.77744, 2.3530) ,
                         new LatLng(48.7776, 2.35361)
                 ));
         // Store a data object with the polyline, used here to indicate an arbitrary type.
         polyline1.setTag("A");
+        Polyline polyline2 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(48.77744, 2.3530),
+                        new LatLng(48.778, 2.35270)
+                ));
+        // Store a data object with the polyline, used here to indicate an arbitrary type.
+        polyline2.setTag("B");
+        Polyline polyline3 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(48.778, 2.35270),
+                        new LatLng(48.77855, 2.35476)
+                ));
+        // Store a data object with the polyline, used here to indicate an arbitrary type.
+        polyline3.setTag("C");
+        Polyline polyline4 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(48.77855, 2.35476),
+                        new LatLng(48.77790, 2.35485)
+                ));
+        // Store a data object with the polyline, used here to indicate an arbitrary type.
+        polyline4.setTag("D");
+        Polyline polyline5 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(48.77790, 2.35485),
+                        new LatLng(48.7776, 2.35361)
+                ));
+        // Store a data object with the polyline, used here to indicate an arbitrary type.
+        polyline5.setTag("E");
 
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(48.7774, 2.3532), 16));
-        LatLng maison = new LatLng(48.77749, 2.35349);
+        LatLng maison = new LatLng(48.7775, 2.3534);
         googleMap.addMarker(new MarkerOptions().position(maison)
-                .title("Mon p'tit chien"));
+                .title("Mon p'tit chien").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(maison));
         LatLng  destination = new LatLng(48.7775, 2.35350);
         googleMap.addMarker(new MarkerOptions().position(destination)
-                .title("Son chien ami, double-cliquez pour l'ajouter à vos amis"));
+                .title("Son chien ami, double-cliquez pour l'ajouter à vos amis").icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(destination));
 
     }
